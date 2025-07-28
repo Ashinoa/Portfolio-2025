c362645e753e7f5d1bcd0785ea046bb9a51cef9e
Guadalupe Arroyo <117480488+Ashinoa@users.noreply.github.com>
2025-07-28 20:04:06 -0300

    backgorund and text init

--- portfolio/src/app/pages/home/home.ts
+++ portfolio/src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from "../../components/header/header";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
