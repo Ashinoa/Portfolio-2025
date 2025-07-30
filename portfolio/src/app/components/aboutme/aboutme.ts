29f0d0160962344f213b284cc9d7234d6e2e44a1
Guadalupe Arroyo <117480488+Ashinoa@users.noreply.github.com>
2025-07-30 20:56:30 -0300

    animations home and fix about me

--- portfolio/src/app/components/aboutme/aboutme.ts
+++ portfolio/src/app/components/aboutme/aboutme.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID, inject, Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap/gsap-core';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-aboutme',
@@ -6,6 +11,32 @@ import { Component } from '@angular/core';
   templateUrl: './aboutme.html',
   styleUrl: './aboutme.css'
 })
-export class Aboutme {
+export class Aboutme implements OnInit {
+
+
+  private platformId = inject(PLATFORM_ID);
+
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        this.startAnimations();
+      }, 0);
+    }
+  }
+
+  startAnimations() {
+    const tl = gsap.timeline({
+      ease: 'power2.out',
+      scrollTrigger: {
+        scrub: 1,
+      },
+    });
+
+    tl.to('#text-aboutme', {duration: 1, scale: 1, x: 0, y: 0, })
+      .to('#brujita', { x: 300, y: -50, duration: 2 },'<')
+      .to('#brujita', { x: 600, y: 50, duration: 3})
+      .to('#brujita', { x: 900, y: -30, duration: 2.5});
+  }
 
 }
--- portfolio/src/app/pages/home/home.ts
+++ portfolio/src/app/pages/home/home.ts
@@ -1,27 +1,53 @@
-import {Component, ChangeDetectorRef } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID, inject, Component, ChangeDetectorRef, OnInit } from '@angular/core';
 import { gsap } from "gsap";
 import { ScrollTrigger } from 'gsap/all';
 import { Start } from "../../components/start/start";
+import { Aboutme } from "../../components/aboutme/aboutme";
 
 gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-home',
-  imports: [Start],
+  imports: [Start, Aboutme],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit {
 
   isLoading = true;
+  private platformId = inject(PLATFORM_ID);
 
-  constructor(private cdr: ChangeDetectorRef) {}
+  constructor(private cdr: ChangeDetectorRef) { }
 
-  changeStateLoad(value: boolean){
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        this.startAnimations();
+      }, 0); //que espere a que Angular renderice el DOM
+    }
+  }
+
+  changeStateLoad(value: boolean) {
     console.log("Cambio de valor isLoading");
     this.isLoading = value;
     this.cdr.detectChanges(); // fuerza a Angular a revisar los cambios después del cambio manual
   }
 
+
+  startAnimations() {
+    const timeline = gsap.timeline({
+      ease: 'power2.out',
+      scrollTrigger: {
+        scrub: 1,
+      },
+    });
+
+    timeline.to('#start', { opacity: 0, duration: 1})
+            .to('#aboutme', { opacity: 100, duration: 3 },'-=0.5');
+
+  }
 }
+
+
 
