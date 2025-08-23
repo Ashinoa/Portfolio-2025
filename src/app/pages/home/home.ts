502b8cd0045df99804106354624ea4f19b08f402
Guadalupe Arroyo <117480488+Ashinoa@users.noreply.github.com>
2025-08-22 23:52:43 -0300

    fixed bug cursor pointer in home component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -18,6 +18,7 @@ export class Home implements OnInit, OnDestroy {
 
   private platformId = inject(PLATFORM_ID);
 
+  projects !: boolean;
 
   constructor(private cdr: ChangeDetectorRef) { }
 
@@ -26,7 +27,7 @@ export class Home implements OnInit, OnDestroy {
       this.cdr.detectChanges();
       setTimeout(() => {
         this.startAnimations();
-        
+
       }, 0); //que espere a que Angular renderice el DOM
     }
   }
@@ -40,20 +41,70 @@ export class Home implements OnInit, OnDestroy {
     const timeline = gsap.timeline({
       ease: 'power2.out',
       scrollTrigger: {
-        scrub: 1,
+        scrub: 1
       },
     });
+
 
     timeline.to('#start', { opacity: 0, duration: 1 })
       .to('#aboutme', { opacity: 100, duration: 2 }, '-=0.08')
       .to('#aboutme', { opacity: 0, duration: 1 })
-      .to('#projects', { opacity: 100, duration: 1 });
+      .to('#projects', { opacity: 100, duration: 1, display: "block", pointerEvents: "auto", 
+        onComplete:() =>{
+        this.onComponentLeave("projects");
+      }, 
+      onReverseComplete:() =>{
+         this.onComponentLeave("projects");
+      } });
   }
 
- 
+  onComponent(idComponent: string) { 
+    const component = document.getElementById(idComponent);
+    console.log("onComponent: ", component, "pointer events: ", component?.style.pointerEvents);
+    if(component){
+      component.style.pointerEvents = 'auto';
+      component.style.display = 'block';
+    }
+    
+  }
+
+  
+  onComponentLeave(idComponent: string) { 
+    const component = document.getElementById(idComponent);
+    console.log("onComponentLeave: ", component, "pointer events: ", component?.style.pointerEvents);
+    if(component){
+      component.style.pointerEvents = 'none';
+      component.style.display = 'none';
+    }
+    
+  }
+
+
+
+  projectsAnimate() {
+    const tl = gsap.timeline({
+      ease: 'power2.out',
+      scrollTrigger: {
+        scrub: 1,
+        onEnter: () => {
+          tl.set('#projects', { display: "block" });
+        },
+        onLeaveBack: () => {
+          tl.set('#projects', { display: "none" });
+        }
+      },
+    });
+
+
+
+  }
+
 
 
 }
+
+
+
 
 
 
